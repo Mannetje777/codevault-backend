71073824f1789ee72682c82e9e426c7253ca8688
Mannetje777 <neysekant2020@gmail.com>
2025-04-10 22:41:48 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify  
+import sqlite3  
+
+app = Flask(__name__)  
+
+def init_db():  
+    conn = sqlite3.connect('snippets.db')  
+    c = conn.cursor()  
+    c.execute('''CREATE TABLE IF NOT EXISTS snippets  
+                 (id INTEGER PRIMARY KEY, title TEXT, language TEXT, code TEXT)''')  
+    conn.commit()  
+    conn.close()
+
+init_db()  
+
+@app.route('/snippets', methods=['GET'])  
+def get_snippets():  
+    conn = sqlite3.connect('snippets.db')  
+    c = conn.cursor()  
+    c.execute("SELECT * FROM snippets")  
+    snippets = [{"id": row[0], "title": row[1], "language": row[2], "code": row[3]} for row in c.fetchall()]  
+    conn.close()
+    return jsonify(snippets)  
+
+@app.route('/snippets', methods=['POST'])  
+def add_snippet():  
+    data = request.json  
+    conn = sqlite3.connect('snippets.db')  
+    c = conn.cursor()  
+    c.execute("INSERT INTO snippets (title, language, code) VALUES (?, ?, ?)",  
+              (data['title'], data['language'], data['code']))  
+    conn.commit()  
+    conn.close()
+    return jsonify({"status": "success"})  
+
+# Deze route voeg je toe voor statistieken:
+@app.route('/stats', methods=['GET'])
+def snippet_stats():
+    conn = sqlite3.connect('snippets.db')
+    c = conn.cursor()
+    c.execute("SELECT language, COUNT(*) FROM snippets GROUP BY language")
+    stats = c.fetchall()
+    conn.close()
+    return jsonify(stats)
+
+if __name__ == '__main__':  
+    app.run(debug=True)
